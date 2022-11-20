331f951b0130fe51ac8940f16c6905b793a40ce8
JeremiasRy <ryttari.jeremias@gmail.com>
2022-11-20 12:31:53 +0200

    feat: add DllImports

--- game-engine/GameUtility/KeyboardControl.cs
+++ game-engine/GameUtility/KeyboardControl.cs
@@ -3,9 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Runtime.InteropServices;
 
 namespace GameEngine.GameUtility;
 
 public static class KeyboardControl
 {
+    [DllImport("user32.dll")]
+    static extern bool GetKeyboardState(byte lpKeyState);
+    [DllImport("user32.dll")]
+    static extern bool GetKeyState();
+
+    public static bool KeyboardKeyDown()
+    {
+        return false;
+    }
+    public static bool KeyPressed(ConsoleKey key)
+    {
+        return false;
+    }
 }
