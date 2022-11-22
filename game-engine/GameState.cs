8d21d40e116e8762b4fd0e3151b783f0782178e8
JeremiasRy <ryttari.jeremias@gmail.com>
2022-11-22 10:47:47 +0200

    feat: game limits and bounce

--- game-engine/GameState.cs
+++ game-engine/GameState.cs
@@ -12,17 +12,29 @@ public static class GameState
     public static void GameTick() //Call screen buffer here!
     {
         Tick++;
+        List<GameObject> _markForDelete = new();
         foreach (GameObject gameObject in _gameObjects)
         {
             gameObject.Move();
-            ScreenBuffer.Draw(gameObject.Y, gameObject.X, gameObject.Draw());
+            if (!gameObject.OffScreenSide && !gameObject.OffScreenTop)
+            {
+                ScreenBuffer.Draw(gameObject.Y, gameObject.X, gameObject.Draw());
+                ScreenBuffer.DrawText(1, 0, $"GameObj {gameObject.Id}, Yforce: {gameObject.YForce}, Xforce {gameObject.XForce}");
+            } else if (gameObject.OffScreenSide)
+            {
+                _markForDelete.Add(gameObject);
+            }
+        }
+        foreach (var gameObj in _markForDelete)
+        {
+            _gameObjects.RemoveAt(_gameObjects.IndexOf(gameObj));
         }
         ScreenBuffer.DrawText(0, 0, $"Current tick {Tick}");
         ScreenBuffer.DrawScreen();
     }
     public static void AddGameObject()
     {
-        _gameObjects.Add(new GameObject(500, -300, 0, Console.WindowHeight - 1));
+        _gameObjects.Add(new GameObject(1, 4000, -11000, 0, Console.WindowHeight - 1));
     }
 
     public static void StartGame() => Running = true;
--- game-engine/Objects/GameObject.cs
+++ game-engine/Objects/GameObject.cs
@@ -10,16 +10,20 @@ public class GameObject
 {
     // All this is for gravity
     // Negative forces go up and left positive down and right,
-    const int MAX_VELOCITY = 5;
-    const int GRAVITY_FORCE = 15;
-    const int FRICTION_FORCE = 1;
-    const int FORCE_TO_INCREASE_VELOCITY = 50;
+    const int MAX_VELOCITY = 50;
+    const int GRAVITY_FORCE = 700;
+    const int FRICTION_FORCE = 12;
+    const int FORCE_TO_INCREASE_VELOCITY = 1400;
+    const int FRICTION_FORCE_GROUND = 600;
     //Gravity ends here
+    public int Id { get; set; }
     public int X { get; private set; }
     public int Y { get; private set; }
+    public bool OffScreenTop { get; private set; }
+    public bool OffScreenSide { get; private set; }
 
-    int XForce { get; set; } = 0;
-    int YForce { get; set; } = 0;
+    public int XForce { get; private set; } = 0;
+    public int YForce { get; private set; } = 0;
 
     int XVelocity => CalculateSpeed(XForce);
     int YVelocity => CalculateSpeed(YForce);
@@ -31,19 +35,30 @@ public class GameObject
 
     public void Move()
     {
-        if (XVelocity != 0)
-            ApplyHorizontalForces();
+        if (XForce != 0)
+            ApplyHorizontalForces((YVelocity == 0 && GroundCollision(Y)) ? FRICTION_FORCE + FRICTION_FORCE_GROUND : FRICTION_FORCE);
 
-        YForce += GRAVITY_FORCE + FRICTION_FORCE;
+        ApplyVerticalForces(GRAVITY_FORCE + FRICTION_FORCE);
         X += XVelocity;
-        Y += YVelocity;
+        Y = Y + YVelocity > Console.WindowHeight - 1 ? Console.WindowHeight - 1 : Y + YVelocity;
+        OffScreenTop = Y < 0;
+        OffScreenSide = X > Console.WindowWidth - 1 || X < 0;
+
+        if (GroundCollision(Y) && YForce > FORCE_TO_INCREASE_VELOCITY)
+        {
+            YForce = 0 - YForce / 2;
+        }
     }
-    void ApplyHorizontalForces()
+    void ApplyHorizontalForces(int frictionAmount)
     {
-        if (XVelocity < 0)
-            XForce = XForce + FRICTION_FORCE > 0 ? 0 : XForce + FRICTION_FORCE;
+        if (XForce < 0)
+            XForce = XForce + frictionAmount >= 0 ? 0 : XForce + frictionAmount;
         else
-            XForce = XForce - FRICTION_FORCE < 0 ? 0 : XForce - FRICTION_FORCE;
+            XForce = XForce - frictionAmount <= 0 ? 0 : XForce - frictionAmount;
+    }
+    void ApplyVerticalForces(int frictionAmount)
+    {
+        YForce += frictionAmount;
     }
 
     static int CalculateSpeed(int force)
@@ -52,6 +67,7 @@ public class GameObject
         speed = speed > MAX_VELOCITY ? MAX_VELOCITY : speed;
         return force < 0 ? 0 - speed : speed;
     }
+    static bool GroundCollision(int yPos) => yPos >= Console.WindowHeight - 1;
 
     /// <summary>
     /// Still needs a graphic object!!
@@ -60,8 +76,9 @@ public class GameObject
     /// <param name="forceVertical"></param>
     /// <param name="x"></param>
     /// <param name="y"></param>
-    public GameObject(int forceHorizontal, int forceVertical, int x, int y)
+    public GameObject(int id, int forceHorizontal, int forceVertical, int x, int y)
     {
+        Id = id;
         X = x;
         Y = y;
         XForce = forceHorizontal;
