bf7b1a234cccf4396bd03a5b3beb6b6292d5737e
JeremiasRy <ryttari.jeremias@gmail.com>
2022-12-08 10:02:05 +0200

    feat: more collision physics... bugs remain

--- game-engine/Objects/BaseClasses/AffectedByForces.cs
+++ game-engine/Objects/BaseClasses/AffectedByForces.cs
@@ -64,8 +64,27 @@ public class AffectedByForces : GameObject
         List<int[]> pathTaken = GameObject.CalculatePath(X, Y, X + x, Y + y);
         X += x;
         Y += y;
-        
-
+        if (GameObjCollision(pathTaken, out AffectedByForces? collidedObj, out int[]? collisionCoordinates))
+        {
+            if (collidedObj is null && collisionCoordinates is not null) //Collision with a solid object
+            {
+                if (Math.Abs(XForce) > FORCE_TO_INCREASE_HORIZONTAL_VELOCITY) 
+                    X = XForce < 0 ? collisionCoordinates[0] + 1 : collisionCoordinates[0] - 1;
+                if (Math.Abs(YForce) > FORCE_TO_INCREASE_VERTICAL_VELOCITY)
+                    Y = YForce < 0 ? collisionCoordinates[1] - 1 : collisionCoordinates[1] + 1;
+                
+                XForce = ReverseForce(XForce) / 2;
+                YForce = ReverseForce(YForce) / 2;
+                return;
+            }
+            else if (collidedObj is not null)
+            {
+                XForce += collidedObj.XForce < 0 ? collidedObj.XForce - FORCE_TO_INCREASE_HORIZONTAL_VELOCITY : collidedObj.XForce + FORCE_TO_INCREASE_HORIZONTAL_VELOCITY;
+                YForce += collidedObj.YForce < 0 ? collidedObj.YForce + FORCE_TO_INCREASE_VERTICAL_VELOCITY : collidedObj.YForce - FORCE_TO_INCREASE_VERTICAL_VELOCITY;
+                collidedObj.XForce /= 2;
+                collidedObj.YForce /= 2;
+            }  
+        }
         if ((SideCollision(X) || SideCollision(X + Width)))
         {
             X = X < Console.WindowWidth / 2 ? 0 : Console.WindowWidth - 1 - Width;
@@ -76,20 +95,7 @@ public class AffectedByForces : GameObject
             Y = Console.WindowHeight - Height;
             YForce = ReverseForce(YForce) / 2;
         } 
-        if (GameObjCollision(pathTaken, out AffectedByForces? collidedObj))
-        {
-            if (collidedObj is null) //Collision with a solid object
-            {
-                XForce = ReverseForce(XForce);
-                YForce = ReverseForce(YForce);
-            } else
-            {
-                XForce += collidedObj.XForce < 0 ? collidedObj.XForce - FORCE_TO_INCREASE_HORIZONTAL_VELOCITY : collidedObj.XForce + FORCE_TO_INCREASE_HORIZONTAL_VELOCITY;
-                YForce += collidedObj.YForce < 0 ? collidedObj.YForce + FORCE_TO_INCREASE_VERTICAL_VELOCITY : collidedObj.YForce - FORCE_TO_INCREASE_VERTICAL_VELOCITY;
-                collidedObj.XForce /= 2;
-                collidedObj.YForce /= 2;
-            }
-        }
+        
     }
     void ApplyHorizontalForces(int frictionAmount)
     {
@@ -115,22 +121,19 @@ public class AffectedByForces : GameObject
         speed = speed > MAX_VELOCITY ? MAX_VELOCITY : speed;
         return force < 0 ? 0 - speed : speed;
     }
-    public bool GameObjCollision(List<int[]> path, out AffectedByForces? collisionObj)
+    public bool GameObjCollision(List<int[]> path, out AffectedByForces? collisionObj, out int[]? collisionCoordinates)
     {
         collisionObj = null;
+        collisionCoordinates = null;
         var mapWithoutMe = GameState.GameObjectsOnMap.Where(arr => arr[^1] != Id);
         if (!mapWithoutMe.Any())
             return false;
 
-        if (path.Count > 2)
-        {
-            var jausers = "jausers";
-        }
-
-        var objectCollision = mapWithoutMe.FirstOrDefault(arr => arr[0] == X && arr[1] == Y);
+        var objectCollision = mapWithoutMe.FirstOrDefault(objectsCoordinates => path.Any(pathCoordinates => objectsCoordinates[0] == pathCoordinates[0] && objectsCoordinates[1] == pathCoordinates[1]));
         if (objectCollision is null)
             return false;
 
+        collisionCoordinates = objectCollision.Take(2).ToArray();
         collisionObj = GameState.FindGameObj(objectCollision[^1]) as AffectedByForces; //Last item of array is objects id
 
         return true;
